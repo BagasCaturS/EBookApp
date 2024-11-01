a4545f0a9e7c9ede5a569a7d3b252ef3c807a28a
BagasCaturS <bagascatursantoso@gmail.com>
2024-11-01 15:55:03 +0700

    created user model

---
+++ server/src/models/user.ts
@@ -0,0 +1,29 @@
+import exp from "constants";
+import { model, Schema } from "mongoose";
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'author', 'admin'],
+        default: 'user'
+    },
+});
+
+
+
+const UserModel = model("User", userSchema)
+
+
+export default UserModel
