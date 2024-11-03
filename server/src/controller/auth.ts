d77d07b231063d6838ec08dc8d800012407ae811
BagasCaturS <bagascatursantoso@gmail.com>
2024-11-04 00:14:28 +0700

    added email to request verification from user added dependencies nodemailer

--- server/src/controller/auth.ts
+++ server/src/controller/auth.ts
@@ -2,6 +2,7 @@ import { Request, Response, RequestHandler } from "express"
 import crypto from "crypto";
 import verificationTokenModel from "@/models/verificationToken";
 import UserModel from "@/models/user";
+import nodemailer from "nodemailer";
 
 
 export const generateAuthLink: RequestHandler = async (req, res) => {
@@ -24,7 +25,31 @@ export const generateAuthLink: RequestHandler = async (req, res) => {
         token: randomToken,
     })
 
+
+    // Looking to send emails in production? Check out our Email API/SMTP product!
+const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "5788f27209e0e8",
+      pass: "540411b54aa366"
+    }
+  });
+
+  const link = `http://localhost:5050/auth/verify?token=${randomToken}&userId=${userId}`
+
+  await transport.sendMail({
+    to: user.email,
+    from: 'bagascatursantoso@gmail.com',
+    subject: 'Please verify your account',
+    html: `
+    <div>
+    <p> Click this <a href="${link}">link </a>to verify your account </p>
+    </div>
+    `
+  })
+
     console.log(req.body)
-    res.json({ ok: true })
+    res.json({ message: "check your email" })
 
 };
